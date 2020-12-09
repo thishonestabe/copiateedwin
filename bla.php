b87b5d4420ef6826b1c9473a134c3d438dc47613
thishonestabe <marquez.abraham1991@gmail.com>
2020-12-09 14:01:08 +0100

    Forkealo adriano

--- bla.php
+++ bla.php
@@ -4,7 +4,7 @@
 $platos = array(0 => array("titulo" => 
 "arepa", "comensales" => 2, 
 "tipo" => "entrada", "ingredients" => 
-array("queso", "jamon", "cebolla")), 1 => array("titulo" => "cachapa", "comensales" => 2, "tipo" => "principal", "ingredients" => array("queso", "jamon", "cebolla")), 2 => array("titulo" => "tequeno", "comensales" => 1, "tipo" => "entrada", "ingredients" => array("queso", "jamon", "cebolla")));
+array("queso", "jamon", "cebolla")), 1 => array("titulo" => "cachapa", "comensales" => 2, "tipo" => "principal", "ingredients" => array(array("nombre: " => "queso", "cantidad" => 2), array("nombre" => "jamon", "cantidad" => 3), array("nombre" => "cebolla", "cantidad" => 4))), 2 => array("titulo" => "tequeno", "comensales" => 1, "tipo" => "entrada", "ingredients" => array(array("nombre"=>"queso", "cantidad"=>2), array("nombre"=>"jamon","cantidad"=>3), array("nombre"=>"cebolla","cantidad"=>4))));
 //var_dump($platos);
 foreach ($platos as $valor) {
     
@@ -22,12 +22,18 @@ foreach ($platos as $valor) {
     <?php }?>
 
     <br/>
+
+
     <li><?php echo 'titulo: ' . $valor['titulo'] ?></li>
     <li><?php echo 'comensales: ' . $valor['comensales'] ?></li>
     <li><?php echo 'tipo: ' . $valor['tipo'] ?></li>
     <li> <p>Ingedientes:</p>
     <?php foreach ($valor['ingredients'] as $ingrediente) { ?>
-        <li><?php echo $ingrediente ?></li>
+        <?php foreach ($ingrediente as $key2 => $value2) { ?>
+        <li><?php echo $key2 . " : " . $value2 ?></li>
+        
+        <?php }?>
+        
     <?php }?>
     </li>
     <br/>
